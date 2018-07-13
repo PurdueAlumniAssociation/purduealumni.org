bad59ebf85c2112740df73a839a05b6de7c3a895
Blake Holaday <bholaday@purdue.edu>
2018-07-13 10:48:14 -0400

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const gulp = require('gulp');
+
+// sass
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+
+// html
+const fileinclude = require('gulp-file-include');
+const htmlPrettify = require('gulp-html-prettify');
+
+// enable live reload
+const connect = require('gulp-connect');
+
+// const del = require('del');
+
+// sass
+gulp.task('sass', function () {
+  gulp.src('src/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('./'))
+
+  .pipe(gulp.dest('dist/css/'))
+  .pipe(connect.reload());
+});
+
+// html
+gulp.task('html', function () {
+  gulp.src(['src/html/**/*.html', '!src/html/partials/'])
+  // include module files
+  .pipe(fileinclude({
+    prefix: '@@',
+    basepath: '@file'
+  }))
+  // clean up html
+  .pipe(htmlPrettify({indent_char:' ',indent_size:4}))
+  .pipe(gulp.dest('dist/html/'))
+  .pipe(connect.reload());
+});
+
+// live reload
+gulp.task('connect', function() {
+    connect.server({
+        root: "dist",
+        livereload: true
+    });
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+  gulp.watch('src/html/**/*.html', ['html']);
+});
+
+gulp.task('default', ['html', 'sass', 'connect', 'watch']);
