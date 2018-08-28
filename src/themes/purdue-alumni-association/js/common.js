fbc2d6aa07605272e6fbd27724b0712f3d4e1e25
Blake Holaday <bholaday@purdue.edu>
2018-08-28 16:02:09 -0400

    Add membership pages as hard-coded templates
    Also enqued scripts and styles

---
+++ src/themes/purdue-alumni-association/js/common.js
@@ -0,0 +1,31 @@
+$(document).ready( function($) {
+    //black bar search toggle
+    $('.black-bar-menu__link--search').click( function() {
+        $('.black-bar-menu__search-form').slideToggle( {
+            duration: 'fast',
+            easing: 'linear',
+            complete: function() {
+                if( $('.black-bar-menu__search-form').css('display') === "none" ) {
+                    $('.black-bar-menu__link--search').focus();
+                } else {
+                    $('#black-bar-search').focus();
+                }
+            }
+        });
+    });
+
+    // mobile menu toggle
+    $('#mobile-menu-toggle').click( function() {
+        $('#mobile-menu').slideToggle( {
+            duration: 'fast',
+            easing: 'linear',
+            complete: function() {
+                if( $('#mobile-menu').css('display') === "none" ) {
+                    $('#mobile-menu-toggle').focus();
+                } else {
+                    $('#mobile-menu.primary-menu__list').focus();
+                }
+            }
+        });
+    });
+});
