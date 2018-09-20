a3214595ee3b8b2f7b2bbf6917b034535b1152bb
Blake Holaday <bholaday@purdue.edu>
2018-09-20 14:48:34 -0400

    update link

--- src/themes/purdue-alumni-association/page-membership-tiers.php
+++ src/themes/purdue-alumni-association/page-membership-tiers.php
@@ -17,8 +17,8 @@
     </div>
     <section class="row row--slim flex flex--space-between">
         <p>
-            <a class="button button--light-gray button--dark-text button--bold mobile-only" href="<?php echo esc_url( home_url( 'membership/membership-plans/frequently-asked-questions#section3' ) ); ?>" target="_top">Current Members</a>
-            <a class="button button--light-gray button--dark-text button--bold not-mobile" href="<?php echo esc_url( home_url( 'membership/membership-plans/frequently-asked-questions#section3' ) ); ?>" target="_top">Current Members - Click here for upgrade info</a>
+            <a class="button button--light-gray button--dark-text button--bold mobile-only" href="<?php echo esc_url( home_url( 'membership/membership-plans/frequently-asked-questions#section2' ) ); ?>" target="_top">Current Members</a>
+            <a class="button button--light-gray button--dark-text button--bold not-mobile" href="<?php echo esc_url( home_url( 'membership/membership-plans/frequently-asked-questions#section2' ) ); ?>" target="_top">Current Members - Click here for upgrade info</a>
         </p>
         <p>
             <a class="button button--light-gray button--dark-text button--bold mobile-only" href="<?php echo esc_url( home_url( 'membership/membership-plans/frequently-asked-questions' ) ); ?>" target="_top">FAQ</a>
