42ca6ca26868ecb4e7e2c8b3061613bac3dbf412
Blake Holaday <bholaday@purdue.edu>
2018-10-05 14:36:53 -0400

    Abstract out lightbox to template-parts

--- src/themes/purdue-alumni-association/footer.php
+++ src/themes/purdue-alumni-association/footer.php
@@ -22,26 +22,7 @@
         <?php endif; ?>
     </div>
 </footer>
-<div id="login-box" class="login-box">
-    <div class="login-box__column-wrapper">
-        <div class="login-box__column login-box__column--info">
-            <h2 class="login-box__title">Your Information</h2>
-            <p>Update your address, manage your profile, or make a payment.</p>
-            <a href="https://tinyurl.com/ybr3yvcm" class="button button--gold login-box__button">Login</a>
-        </div>
-        <div class="login-box__column login-box__column--benefits">
-            <h2 class="login-box__title">Membership Benefits</h2>
-            <p>Access discounts, career services, or any benefit of your Purdue Alumni Association membership.</p>
-            <a href="https://purdue.vineup.com/" class="button button--teal login-box__button">Login</a>
-        </div>
-        <div class="login-box__column login-box__column--desc">
-            <h2 class="login-box__title login-box__title--desc">Why two logins?</h2>
-            <p>Purdue Alumni is transitioning technology platforms to enhance the online experience. Soon, you will have one login for everything. Until then, don't hesitstate to call or email for assistance.</p>
-            <p>800-414-1541<br />
-            <a href="mailto:purduealumni@purdue.edu">purduealumni@purdue.edu</a></p>
-        </div>
-    </div>
-</div>
+<?php get_template_part( 'template-parts/login-lightbox' ); ?>
 <?php wp_footer(); ?>
 </body>
 </html>
