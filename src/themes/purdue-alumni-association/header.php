fe9959b7774dcbfd6f27d90fe20f690612d0ecfb
Blake Holaday <bholaday@purdue.edu>
2018-11-05 14:37:40 -0500

    Update formatting

--- src/themes/purdue-alumni-association/header.php
+++ src/themes/purdue-alumni-association/header.php
@@ -11,13 +11,13 @@
 
             $clean_total = str_replace( '$', '', $_GET['total'] );
         ?>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            dataLayer.push({
-                'transactionId': '<?= $transaction_id; ?>',
-                'transactionTotal': <?= $clean_total; ?>
-            });
-        </script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  dataLayer.push({
+    'transactionId': '<?= $transaction_id; ?>',
+    'transactionTotal': <?= $clean_total; ?>
+  });
+</script>
     <?php endif; ?>
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KTNT5LJ');</script>
