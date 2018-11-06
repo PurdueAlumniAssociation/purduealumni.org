0f37897d7e21c3b55ac5973df5d44631a79ab651
Blake Holaday <bholaday@purdue.edu>
2018-11-06 14:00:21 -0500

    Test page for referer

---
+++ src/special-pages/ecommerce-test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Test</title>
+    </head>
+    <body>
+        <a href="https://dev.purduealumni.org/ecommerce-passthrough.php">Test w/o params</a><br />
+        <a href="https://dev.purduealumni.org/ecommerce-passthrough.php?total=%2439.00&fid=1&eid=1">Test w/ params</a><br />
+        <a href="https://dev.purduealumni.org/ecommerce-passthrough.php?total=%2439.00&fid=1&eid=1&product=GradUpgradeToPro">Test w/ params + product</a><br />
+        <a href="https://dev.purduealumni.org/ecommerce-passthrough.php?total=%2439.00&fid=1&eid=1&purchaser=other">Test w/ params + purchaser</a><br />
+    </body>
+</html>
