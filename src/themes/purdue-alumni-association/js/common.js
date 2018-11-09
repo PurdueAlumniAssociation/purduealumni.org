716bf48c0fb8a101778cfb963f9af6156ec0ee3a
Blake Holaday <bholaday@purdue.edu>
2018-11-09 15:05:29 -0500

    Add cookie logic and change appropriate links

--- src/themes/purdue-alumni-association/js/common.js
+++ src/themes/purdue-alumni-association/js/common.js
@@ -1,3 +1,19 @@
+(function paaCookies() {
+    $.urlParam = function(name){
+        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        if ( results !== null ) {
+            return results[1];
+        } else {
+            return 0;
+        }
+    }
+
+    if ( $.urlParam('utm_campaign') ) {
+        Cookies.set( 'utm_campaign', $.urlParam('utm_campaign'), { expires: 30 } );
+    }
+}());
+
+
 $(document).ready( function($) {
     //black bar search toggle
     $('.black-bar-menu__link--search').click( function() {
@@ -28,4 +44,19 @@ $(document).ready( function($) {
             }
         });
     });
+
+    // check for a campaign cookie
+    if ( Cookies.get('utm_campaign') !== undefined ) {
+        // change links to ecommerce forms
+        $('[rel~=ecommerce]').each(function() {
+            var href = $( this ).attr("href");
+            // check to see if the URL already has some query params or not
+            if ( href.indexOf("?") == -1 ) {
+                $( this ).attr("href", href + "?utm_campaign=" + Cookies.get('utm_campaign'))
+            } else {
+                $( this ).attr("href", href + "&utm_campaign=" + Cookies.get('utm_campaign'))
+            }
+        });
+    }
+
 });
