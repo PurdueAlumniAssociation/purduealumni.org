41ddcb6148b1a2d7df6b7f3d418cdbc798ef52a4
Blake Holaday <bholaday@purdue.edu>
2018-11-15 12:31:05 -0500

    Rename 'utm_campaign' to 'campaign'

--- src/themes/purdue-alumni-association/js/common.js
+++ src/themes/purdue-alumni-association/js/common.js
@@ -46,17 +46,16 @@ $(document).ready( function($) {
     });
 
     // check for a campaign cookie
-    if ( Cookies.get('utm_campaign') !== undefined ) {
+    if ( Cookies.get('campaign') !== undefined ) {
         // change links to ecommerce forms
         $('[rel~=ecommerce]').each(function() {
             var href = $( this ).attr("href");
             // check to see if the URL already has some query params or not
             if ( href.indexOf("?") == -1 ) {
-                $( this ).attr("href", href + "?utm_campaign=" + Cookies.get('utm_campaign'))
+                $( this ).attr("href", href + "?campaign=" + Cookies.get('campaign'))
             } else {
-                $( this ).attr("href", href + "&utm_campaign=" + Cookies.get('utm_campaign'))
+                $( this ).attr("href", href + "&campaign=" + Cookies.get('campaign'))
             }
         });
     }
-
 });
