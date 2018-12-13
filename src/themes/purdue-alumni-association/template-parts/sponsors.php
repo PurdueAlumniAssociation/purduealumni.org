b6e178489627b3ce391401199d51359882db9a8d
Blake Holaday <bholaday@purdue.edu>
2018-12-13 09:17:44 -0500

    Remove comments

--- src/themes/purdue-alumni-association/template-parts/sponsors.php
+++ src/themes/purdue-alumni-association/template-parts/sponsors.php
@@ -1,14 +1,3 @@
-<!-- <div class="sponsors">
-    <a class="sponsors__link" target="_blank" rel="noopener" href="https://www.balfour.com/purdue">
-        <img class="sponsors__image" src="<?php //echo esc_url( home_url( 'wp-content/uploads/balfour.svg' ) ); ?>" alt="Balfour" />
-    </a>
-    <a class="sponsors__link" target="_blank" rel="noopener" href="https://www.purduefed.com/personal/cards/credit-cards/">
-        <img class="sponsors__image" src="<?php //echo esc_url( home_url( 'wp-content/uploads/PurdueFed18.svg' ) ); ?>" alt="PEFCU" />
-    </a>
-    <a class="sponsors__link" target="_blank" rel="noopener" href="https://www.libertymutual.com/purdue">
-        <img class="sponsors__image" src="<?php //echo esc_url( home_url( 'wp-content/uploads/liberty-mutual.svg' ) ); ?>" alt="Liberty Mutual Insurance" />
-    </a>
-</div> -->
 <div class="bootstrap-row sponsors">
     <a class="col-xs-12 col-sm-4 sponsors__link" target="_blank" rel="noopener" href="https://www.balfour.com/purdue">
         <img class="sponsors__image" src="<?php echo esc_url( home_url( 'wp-content/uploads/balfour.svg' ) ); ?>" alt="Balfour" />
