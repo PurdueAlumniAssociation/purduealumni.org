bf085f681eb3749c0791dcbfac74cb8260d6938a
Blake Holaday <bholaday@purdue.edu>
2019-01-10 12:25:50 -0500

    Update to reflect new naming convetion and logic

--- src/themes/purdue-alumni-association/template-parts/homepage-hero.php
+++ src/themes/purdue-alumni-association/template-parts/homepage-hero.php
@@ -2,43 +2,51 @@
 $hero_banner_id = rwmb_meta( 'homepage__hero_banner' );
 
 if ( $hero_banner_id ) {
-    //echo print_r( $hero_banner_id );
-    $hero_heading = rwmb_meta( 'hero_banner__heading', '', $hero_banner_id );
-    $hero_content = rwmb_meta( 'hero_banner__content', '', $hero_banner_id );
-    $hero_url = rwmb_meta( 'hero_banner__url', '', $hero_banner_id );
-    $hero_button_label = rwmb_meta( 'hero_banner__button_label', '', $hero_banner_id );
-    $hero_new_tab = rwmb_meta( 'hero_banner__new_tab', '', $hero_banner_id );
-    $hero_background_image = rwmb_meta( 'hero_banner__background_image', array( 'limit' => 1 ), $hero_banner_id );
-    $hero_dark_overlay = rwmb_meta( 'hero_banner__dark_overlay', '', $hero_banner_id );
+    $hero_heading = rwmb_meta( 'heading', '', $hero_banner_id );
+    $hero_content = rwmb_meta( 'content', '', $hero_banner_id );
+    $hero_button_url = rwmb_meta( 'button_url', '', $hero_banner_id );
+    $hero_button_label = rwmb_meta( 'button_label', '', $hero_banner_id );
+    $hero_button_target = rwmb_meta( 'button_target', '', $hero_banner_id );
+    $hero_background_image = rwmb_meta( 'background_image', $hero_banner_id );
+    $hero_background_options = rwmb_meta( 'background_options', '', $hero_banner_id );
 
     $dark_overlay = '';
-    if ( $hero_dark_overlay ) {
+    if ( isset( $hero_background_options ) && in_array( 'dark', $hero_background_options ) {
         $dark_overlay = "<div class=\"homepage-hero__dark-layer\"></div>\n";
     }
 
     $target = '';
-    if ( $hero_new_tab ) {
-        $target = ' target="_blank" rel="noopener"';
+    if ( isset( $hero_button_target ) ) {
+        $target = " target=\"${hero_button_target}\"";
+
+        if ( $hero_button_target == "_blank" ) {
+            $target .= " rel=\"noopener\"";
+        }
     }
 
     // get image
-    $image = $hero_background_image[0];
-    $img_src = $image['full_url'];
+    $hero_img_src = $hero_background_image['full_url'];
+    $hero_img_alt =  $hero_background_image['alt'];
 
     echo "<section class=\"row row--no-padding\">
             <div class=\"homepage-hero\">
-                <img class=\"homepage-hero__image\"{$target} src=\"{$img_src}\" />
+                <img class=\"homepage-hero__image\" src=\"{$hero_img_src}\" alt=\"${$hero_img_alt}\" />
                 {$dark_overlay}
                 <div class=\"homepage-hero__content-container\">
                     <div class=\"homepage-hero__primary\">
-                        <h1>{$hero_heading}</h1>
-                        <a class=\"button button--gold\" href=\"{$hero_url}\">{$hero_button_label}</a>
-                        <p>{$hero_content}</p>
-                    </div>
+                        <h1>{$hero_heading}</h1>";
+
+    if ( isset( $hero_button_label ) && isset( $hero_button_url ) ) {
+                   echo "<a class=\"button button--gold\" href=\"{$hero_url}\"{$target}>{$hero_button_label}</a>";
+    }
+
+    if ( isset( $hero_content ) ) {
+                   echo "<p>{$hero_content}</p>";
+    }
+    echo "          </div>
                 </div>
             </div>
         </section>";
 } else {
-    // fallback hero
-
+    // fallback
 }
