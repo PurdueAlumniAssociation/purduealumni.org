ff7a1f95a563b3cee1ba7eec3723f77c8bbe060a
Blake Holaday <bholaday@purdue.edu>
2019-01-10 12:39:02 -0500

    remove namespacing

--- src/themes/purdue-alumni-association/template-parts/homepage-hero.php
+++ src/themes/purdue-alumni-association/template-parts/homepage-hero.php
@@ -1,52 +1,58 @@
 <?php
-$hero_banner_id = rwmb_meta( 'homepage__hero_banner' );
+$id = rwmb_meta( 'homepage__hero_banner' );
 
-if ( isset( $hero_banner_id ) ) {
-    $hero_heading = rwmb_meta( 'heading', '', $hero_banner_id );
-    $hero_content = rwmb_meta( 'content', '', $hero_banner_id );
-    $hero_button_url = rwmb_meta( 'button_url', '', $hero_banner_id );
-    $hero_button_label = rwmb_meta( 'button_label', '', $hero_banner_id );
-    $hero_button_target = rwmb_meta( 'button_target', '', $hero_banner_id );
-    $hero_background_image = rwmb_meta( 'background_image', $hero_banner_id );
-    $hero_background_options = rwmb_meta( 'background_options', '', $hero_banner_id );
+function get_hero_banner( $id ) {
+    if ( isset( $id ) ) {
+        $heading = rwmb_meta( 'heading', '', $id );
+        $content = rwmb_meta( 'content', '', $id );
+        $button_url = rwmb_meta( 'button_url', '', $id );
+        $button_label = rwmb_meta( 'button_label', '', $id );
+        $button_target = rwmb_meta( 'button_target', '', $id );
+        $background_image = rwmb_meta( 'background_image', $id );
+        $background_options = rwmb_meta( 'background_options', '', $id );
 
-    $dark_overlay = '';
-    if ( isset( $hero_background_options ) && in_array( 'dark', $hero_background_options ) {
-        $dark_overlay = "<div class=\"homepage-hero__dark-layer\"></div>\n";
-    }
-
-    $target = '';
-    if ( isset( $hero_button_target ) ) {
-        $target = " target=\"${hero_button_target}\"";
-
-        if ( $hero_button_target == "_blank" ) {
-            $target .= " rel=\"noopener\"";
+        $dark_overlay = '';
+        if ( isset( $background_options ) && in_array( 'dark', $background_options ) {
+            $dark_overlay = "<div class=\"homepage-hero__dark-layer\"></div>\n";
         }
-    }
 
-    // get image
-    $hero_img_src = $hero_background_image['full_url'];
-    $hero_img_alt =  $hero_background_image['alt'];
+        $target = '';
+        if ( isset( $button_target ) ) {
+            $target = " target=\"${hero_button_target}\"";
 
-    echo "<section class=\"row row--no-padding\">
-            <div class=\"homepage-hero\">
-                <img class=\"homepage-hero__image\" src=\"{$hero_img_src}\" alt=\"${$hero_img_alt}\" />
-                {$dark_overlay}
-                <div class=\"homepage-hero__content-container\">
-                    <div class=\"homepage-hero__primary\">
-                        <h1>{$hero_heading}</h1>";
+            if ( $button_target == "_blank" ) {
+                $target .= " rel=\"noopener\"";
+            }
+        }
 
-    if ( isset( $hero_button_label ) && isset( $hero_button_url ) ) {
-                   echo "<a class=\"button button--gold\" href=\"{$hero_url}\"{$target}>{$hero_button_label}</a>";
-    }
+        // get image
+        $img_src = $background_image['full_url'];
+        $img_alt =  $background_image['alt'];
 
-    if ( isset( $hero_content ) ) {
-                   echo "<p>{$hero_content}</p>";
-    }
-    echo "          </div>
+        // build output
+        $output = "<section class=\"row row--no-padding\">
+                <div class=\"homepage-hero\">
+                    <img class=\"homepage-hero__image\" src=\"{$img_src}\" alt=\"${$img_alt}\" />
+                    {$dark_overlay}
+                    <div class=\"homepage-hero__content-container\">
+                        <div class=\"homepage-hero__primary\">
+                            <h1>{$heading}</h1>";
+
+        if ( isset( $button_label ) && isset( $button_url ) ) {
+            $output .= "<a class=\"button button--gold\" href=\"{$url}\"{$target}>{$button_label}</a>";
+        }
+
+        if ( isset( $content ) ) {
+            $output .= "<p>{$content}</p>";
+        }
+
+        $output .=     "</div>
+                    </div>
                 </div>
-            </div>
-        </section>";
-} else {
-    // fallback
+            </section>";
+    } else {
+        // fallback
+    }
+
+    return $output;
 }
