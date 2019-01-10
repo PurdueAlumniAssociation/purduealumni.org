a9a406119dd9492b407e4724e47b2a04971ac355
Blake Holaday <bholaday@purdue.edu>
2019-01-10 13:20:26 -0500

    Fixes

--- src/themes/purdue-alumni-association/template-parts/homepage-hero.php
+++ src/themes/purdue-alumni-association/template-parts/homepage-hero.php
@@ -6,7 +6,7 @@ function output_hero_banner( $id ) {
         $button_url = rwmb_meta( 'button_url', '', $id );
         $button_label = rwmb_meta( 'button_label', '', $id );
         $button_target = rwmb_meta( 'button_target', '', $id );
-        $background_image = rwmb_meta( 'background_image', $id );
+        $background_image = rwmb_meta( 'background_image', '', $id );
         $background_options = rwmb_meta( 'background_options', '', $id );
 
         $dark_overlay = '';
@@ -15,29 +15,29 @@ function output_hero_banner( $id ) {
         }
 
         $target = '';
-        if ( isset( $button_target ) && $button_target == "1" ) {
-            $target = " target=\"${hero_button_target}\" rel=\"noopener\"";
+        if ( isset( $button_target ) && $button_target == true ) {
+            $target = " target=\"_blank\" rel=\"noopener\"";
         }
 
         // get image
         $img_src = $background_image['full_url'];
-        $img_alt =  $background_image['alt'];
+        $img_alt = $background_image['alt'];
 
         // build output
         $output = "<section class=\"row row--no-padding\">
                 <div class=\"homepage-hero\">
-                    <img class=\"homepage-hero__image\" src=\"{$img_src}\" alt=\"${$img_alt}\" />
-                    {$dark_overlay}
+                    <img class=\"homepage-hero__image\" src=\"${img_src}\" alt=\"${img_alt}\" />
+                    ${dark_overlay}
                     <div class=\"homepage-hero__content-container\">
                         <div class=\"homepage-hero__primary\">
-                            <h1>{$heading}</h1>";
+                            <h1>${heading}</h1>";
 
-        if ( isset( $button_label ) && isset( $button_url ) ) {
-            $output .= "<a class=\"button button--gold\" href=\"{$url}\"{$target}>{$button_label}</a>";
+        if ( isset( $button_label ) && !empty( $button_label ) && isset( $button_url ) && !empty( $button_url ) ) {
+            $output .= "<a class=\"button button--gold\" href=\"${url}\"${target}>${button_label}</a>";
         }
 
         if ( isset( $content ) ) {
-            $output .= "<p>{$content}</p>";
+            $output .= "<p>${content}</p>";
         }
 
         $output .=     "</div>
@@ -46,8 +46,9 @@ function output_hero_banner( $id ) {
             </section>";
     } else {
         // fallback
+        $output = '';
     }
 
-    return $output;
+    echo $output;
 }
 output_hero_banner( rwmb_meta( 'homepage__hero_banner' ) );
