c9e9f913551cb0620de98dd6de33390d4ac09ad3
Blake Holaday <bholaday@purdue.edu>
2019-01-10 15:37:26 -0500

    Account for loop output

--- src/themes/purdue-alumni-association/template-parts/news-events.php
+++ src/themes/purdue-alumni-association/template-parts/news-events.php
@@ -1,5 +1,7 @@
 <?php
 function output_news_events( $ids ) {
+    $output = '';
+
     if ( isset( $ids ) ) {
         foreach ( $ids as $id ) {
             $title = rwmb_meta( 'title', '', $id );
@@ -7,7 +9,7 @@ function output_news_events( $ids ) {
             $button_url = rwmb_meta( 'button_url', '', $id );
             $button_label = rwmb_meta( 'button_label', '', $id );
             $button_target = rwmb_meta( 'button_target', '', $id );
-            $image = rwmb_meta( 'thumbnail_image', '', $id );
+            $image = rwmb_meta( 'image', '', $id );
 
             $target = '';
             if ( isset( $button_target ) && $button_target == true ) {
@@ -18,14 +20,14 @@ function output_news_events( $ids ) {
             $img_src = $image['full_url'];
             $img_alt = $image['alt'];
 
-            $output = "<div class=\"news-event\">
+            $output .= "<div class=\"news-event\">
                 <img class=\"news-event__image\" src=\"${img_src}\" alt=\"${img_alt}\" />
                 <div>
                     <h3 class=\"news-event__title\">${title}</h3>
                     <p class=\"news-event__description\">${description}</p>";
 
             if ( isset( $button_label ) && !empty( $button_label ) && isset( $button_url ) && !empty( $button_url ) ) {
-                $output .= "<a class=\"button\" class=\"news-event__cta\" href=\"${url}\"${target}>${button_label}</a>";
+                $output .= "<a class=\"button\" class=\"news-event__cta\" href=\"${button_url}\"${target}>${button_label}</a>";
             }
 
             $output .= "</div>
