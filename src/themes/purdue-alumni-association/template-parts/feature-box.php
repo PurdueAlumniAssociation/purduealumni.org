a14eef7d2b9f974dd6e841a96916f99df42491f3
Blake Holaday <bholaday@purdue.edu>
2019-01-15 16:07:12 -0500

    Merge branch 'dev' into feat/add_trip-cpt

---
+++ src/themes/purdue-alumni-association/template-parts/feature-box.php
@@ -0,0 +1,38 @@
+<?php
+function output_feature_box( $id ) {
+    if ( isset( $id ) ) {
+        $title = rwmb_meta( 'title', '', $id );
+        $content = rwmb_meta( 'content', '', $id );
+        $button_label = rwmb_meta( 'button_label', '', $id );
+        $button_url = rwmb_meta( 'button_url', '', $id );
+        $button_target = rwmb_meta( 'button_target', '', $id );
+        $image = rwmb_meta( 'image', '', $id );
+
+        $target = '';
+        if ( isset( $button_target ) && $button_target == true ) {
+            $target = ' target="_blank" rel="nofollow"';
+        }
+
+        // get image
+        $img_src = $image['full_url'];
+        $img_alt = $image['alt'];
+
+        $output = "<aside class=\"feature-box\">
+                <img class=\"feature-box__image\" src=\"${img_src}\" alt=\"${img_alt}\">
+                <div class=\"feature-box__content\">
+                    <p class=\"feature-box__text\">${content}</p>";
+
+        if ( isset( $button_label ) && !empty( $button_label ) && isset( $button_url ) && !empty( $button_url ) ) {
+            $output .= "<p><a class=\"button button--almost-black\" href=\"${button_url}\"${target}>${button_label}</a></p>";
+        }
+
+        $output .= "</div>
+            </aside>";
+    } else {
+        // fallback
+        $output = '';
+    }
+
+    echo $output;
+}
+output_feature_box( rwmb_meta( 'feature_box' ) );
---
+++ src/themes/purdue-alumni-association/template-parts/graphic-boxes.php
@@ -0,0 +1,48 @@
+<?php
+function output_graphic_boxes( $ids ) {
+    $colors = array( "green", "orange", "dark-gray" );
+    $color_index = 0;
+    $output = '';
+
+    if ( $ids ) {
+        // $output = "<section class=\"row row--no-padding front-page__graphic-boxes\">\n
+        //         <div class=\"big-info-block\">\n
+        //             <div class=\"big-info-block__row bootstrap-row\" style=\"margin-left: 0; margin-right: 0;\">\n";
+        $output = "<section class=\"row bootstrap-row row--no-padding front-page__graphic-box-row\">\n";
+
+        foreach( $ids as $id ) {
+            $title = rwmb_meta( 'title', '', $id );
+            $cut_line = rwmb_meta( 'cut_line', '', $id );
+            $url = rwmb_meta( 'url', '', $id );
+            $new_tab = rwmb_meta( 'new_tab', '', $id );
+            $background_image = rwmb_meta( 'background_image', '', $id );
+            $color = $colors[$color_index];
+
+            $target = '';
+            if ( isset( $target ) && $target == true ) {
+                $target = " target=\"_blank\" rel=\"nofollow\"";
+            }
+
+            // get image
+            $img_src = $background_image['full_url'];
+
+            $output .= "<a class=\"col-xs-12 col-md-4 front-page__graphic-box-wrapper\"${target} href=\"${url}\">
+                <div class=\"graphic-box graphic-box--${color}\" style=\"background-image: url('${img_src}')\">
+                    <div class=\"graphic-box__content\">
+                        <span class=\"graphic-box__category graphic-box__category--${color}\">${title}</span>
+                        <span class=\"graphic-box__title\">${cut_line}</span>
+                    </div>
+                </div>
+            </a>";
+
+            $color_index++;
+        }
+        $output .= "</section>\n";
+    } else {
+        // fallback
+        $output = '';
+    }
+
+    echo $output;
+}
+output_graphic_boxes( rwmb_meta( 'graphic_boxes' ) );
