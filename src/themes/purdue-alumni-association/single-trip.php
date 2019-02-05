9b84802f254213857594f51b65e7c8183625a0d6
Blake Holaday <bholaday@purdue.edu>
2019-02-05 15:23:17 -0500

    Include display logic on side layout too

--- src/themes/purdue-alumni-association/single-trip.php
+++ src/themes/purdue-alumni-association/single-trip.php
@@ -12,6 +12,14 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
             <section class="row row--no-padding">
                 <img class="banner" src="<?php the_post_thumbnail_url(); ?>" alt="<?= $image_alt ?>" />
             </section>
+
+            <?php
+                $start_date = rwmb_meta( 'start_date', $args );
+                $end_date = rwmb_meta( 'end_date', $args );
+                // $year = date( 'Y', $start_date );
+                //
+                // echo "<a href=\"https://dev.purduealumni.org/trip/?trip-year={$year}\"><i class=\"fas fa-arrow-left\" aria-hidden style=\"margin-right: .2em;\"></i>Back to {$year} trips</a>";
+            ?>
 <?php } ?>
     <?php if ( is_active_sidebar( 'left-sidebar' ) ) : ?>
         <section class="row">
@@ -19,7 +27,41 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
                 <div class="layout__main">
                     <main class="" id="main" tabindex="-1">
                         <h1><?php the_title(); ?></h1>
+                        <p class="trip__date"><?= date( 'F j', $start_date ), "&ndash;", date( 'j, Y', $end_date); ?></p>
                         <?php the_content(); ?>
+                        <?php
+                        $operator = rwmb_meta( 'operator', $args );
+                        if ( ! empty( $operator ) ) {
+                            echo "<p class=\"trip__operator\">Tour Operator: {$operator}</p>";
+                        }
+
+                        $pricing = rwmb_meta( 'pricing', $args );
+                        if ( ! empty( $pricing ) ) {
+                            echo "<p class=\"trip__pricing\">Pricing: {$pricing}</p>";
+                        }
+
+                        $download_array = array();
+                        $download_group = rwmb_meta( 'download_group', $args );
+
+                        foreach ( $download_group as $download ) {
+                            if ( ! empty( $download['download_label'] ) && count( $download['download_upload'] ) > 0 ) {
+                                $download_url = wp_get_attachment_url( $download['download_upload'][0] );
+                                $download_label = $download['download_label'];
+
+                                $download_array[] = "<li><a href=\"{$download_url}\" download>{$download_label}</a></li>";
+                            }
+                        }
+
+                        if ( count( $download_array ) > 0 ) {
+                            echo "<h2>Downloads</h2>";
+                            echo "<ul>";
+                            foreach ( $download_array as $list_item ) {
+                                echo $list_item;
+                            }
+                            echo "</ul>";
+                        }
+
+                        ?>
                     </main>
                 </div>
                 <aside class="layout__sidebar">
@@ -28,15 +70,6 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
             </div>
         </section>
     <?php else : ?>
-        <!-- <section class="row row--slim"> -->
-            <?php
-                $start_date = rwmb_meta( 'start_date', $args );
-                $end_date = rwmb_meta( 'end_date', $args );
-                // $year = date( 'Y', $start_date );
-                //
-                // echo "<a href=\"https://dev.purduealumni.org/trip/?trip-year={$year}\"><i class=\"fas fa-arrow-left\" aria-hidden style=\"margin-right: .2em;\"></i>Back to {$year} trips</a>";
-            ?>
-        <!-- </section> -->
         <section class="row">
             <main id="main" tabindex="-1">
                 <h1><?php the_title(); ?></h1>
