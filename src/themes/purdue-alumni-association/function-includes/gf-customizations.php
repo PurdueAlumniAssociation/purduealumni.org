f4f61f810ea3d4a96e2f27169e22e2ae7dcbf44b
Blake Holaday <bholaday@purdue.edu>
2019-02-22 09:48:13 -0500

    Add rule to restrict purdue.edu domain

---
+++ src/themes/purdue-alumni-association/function-includes/gf-customizations.php
@@ -0,0 +1,10 @@
+<?php
+
+// exclude purdue.edu from email
+new GWEmailDomainControl(array(
+    'form_id' => 30,
+    'field_id' => 2,
+    'domains' => array('purdue.edu'),
+    'validation_message' => __('Whoops! At some point Purdue will delete your <strong>%s</strong> email account, and we want to be able to stay in touch afterwards. Please provide an alternate email address.'),
+    'mode' => 'ban'
+));
