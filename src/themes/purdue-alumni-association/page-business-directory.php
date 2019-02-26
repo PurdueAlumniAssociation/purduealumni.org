d08401d41045bf917ce8804dddc79cb6ae0383e7
Blake Holaday <bholaday@purdue.edu>
2019-02-26 09:05:02 -0500

    Abstract out template part

--- src/themes/purdue-alumni-association/page-business-directory.php
+++ src/themes/purdue-alumni-association/page-business-directory.php
@@ -17,8 +17,7 @@
             <div class="layout">
                 <div class="layout__main">
                     <main class="" id="main" tabindex="-1">
-                        <h1><?php the_title(); ?></h1>
-                        <?php the_content(); ?>
+                        <?php get_template_part( 'template-parts/business-directory-content' ); ?>
                     </main>
                 </div>
                 <aside class="layout__sidebar">
@@ -29,47 +28,7 @@
     <?php else : ?>
         <section class="row">
             <main id="main" tabindex="-1">
-                <h1><?php the_title(); ?></h1>
-                <?php the_content(); ?>
-                <?php
-                    // query
-                    $the_query = new WP_Query(array(
-                        'post_type'         => 'business-listing',
-                        'posts_per_page'    => -1,
-                        'orderby'           => 'title',
-                        'order'             => 'ASC'
-                    ));
-
-                    if ( $the_query->have_posts() ) {
-                        while ( $the_query->have_posts() ) {
-                            $the_query->the_post();
-
-                            $title = get_the_title();
-                            $listing_type = rwmb_meta( 'listing_type' );
-                            $contact_name = rwmb_meta( 'contact_name' );
-                            $contact_email = rwmb_meta( 'contact_email' );
-                            $website = rwmb_meta( 'website' );
-                            $logo = rwmb_meta( 'logo' );
-
-                            echo "<div class=\"directory-listing\">";
-
-                            if ( $listing_type == "Advanced" ) {
-                                echo "<img src=\"${logo['full_url']}\" alt=\"${logo['alt']}\"  class=\"directory-listing__logo\" />";
-                            }
-
-                            echo "<h3 class=\"directory-listing__title\">${title}</h3>
-                                <p><span class=\"directory-listing__name\">${contact_name}</span> - <span class=\"directory-listing__email\"><a href=\"mailto:${contact_email}\">${contact_email}</a></span></p>";
-
-                            if ( $listing_type == "Advanced" ) {
-                                echo "<p class=\"directory-listing__website\"><a href=\"${website}\">${website}</a></p>";
-                            }
-
-                            echo "</div>";
-                        }
-                    } else {
-                        echo "<p>No businesses found!</p>";
-                    }
-                    ?>
+                <?php get_template_part( 'template-parts/business-directory-content' ); ?>
             </main>
         </section>
     <?php endif; ?>
