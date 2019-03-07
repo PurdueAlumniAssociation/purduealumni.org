09039486b411d3dbb207e4a2686d917e4e42ce72
Merriken <gmerrike@1alu-dau2230-a1.csds.purdue.edu>
2019-03-07 13:58:54 -0500

    Added contact information

--- src/themes/purdue-alumni-association/template-parts/trip-content.php
+++ src/themes/purdue-alumni-association/template-parts/trip-content.php
@@ -39,4 +39,6 @@ if ( count( $download_array ) > 0 ) {
 }
 
 ?>
+<h2>Next Steps</h2>
+<p>We will happily send you brochures listing included features, itineraries, and pricing. Questions or ready to make a reservation? We’re just a phone call away!  Call Mary MacDonald at 800-414-1541 or 765-494-5175, or email alumnitravel@purdue.edu.</p>
 </div>
