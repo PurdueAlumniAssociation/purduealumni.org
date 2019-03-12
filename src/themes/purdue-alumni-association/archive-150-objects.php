49eaabe63a47cdfb4d2f2bedfd330133163ab97a
Blake Holaday <bholaday@purdue.edu>
2019-03-12 11:07:48 -0400

    Initial add of templates

---
+++ src/themes/purdue-alumni-association/archive-150-objects.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The template for displaying archive of 150 objects
+ */
+?>
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
---
+++ src/themes/purdue-alumni-association/single-150-object.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The Template for displaying all single 150 objects
+ */
+?>
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
