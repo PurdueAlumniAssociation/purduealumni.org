39825cf8671fdeb70d0de04b7b58fbb40e96e831
Blake Holaday <bholaday@purdue.edu>
2019-03-15 14:51:30 -0400

    Try using the standard 'card' style

--- src/themes/purdue-alumni-association/template-parts/150-item-card.php
+++ src/themes/purdue-alumni-association/template-parts/150-item-card.php
@@ -1,8 +1,30 @@
-<div class="col-xs-12 col-sm-6 col-md-4">
-    <div class="p150-object">
-        <div class="p150-object__image-container">
-            <img class="p150-object__image" src="http://via.placeholder.com/300x300" alt="" width="300" height="300" />
+<!-- <div class="col-xs-12 col-sm-6 col-md-4">
+    <div class="p150-item-card">
+        <div class="p150-item-card__image-container">
+            <?php /*echo get_the_post_thumbnail(
+                $post_id,
+                'Medium-Large',
+                array(
+                    'class' => 'p150-item-card__image',
+                    'width' => '500',
+                    'height' => '500'
+                )
+            ); */?>
         </div>
-        <h3 class="p150-object__title">Title of the Object TALL LETTERS GO HERE</h3>
+        <h3 class="p150-item-card__title"><?php //the_title(); ?></h3>
     </div>
-</div>
+</div> -->
+<a href="#">
+    <div class="card">
+        <?php echo get_the_post_thumbnail(
+            $post_id,
+            'Medium-Large',
+            array(
+                'class' => 'card__image'
+            )
+        ); ?>
+        <div class="card__content">
+            <h3 class="card__title"><?php the_title(); ?></h3>
+        </div>
+    </div>
+</a>
