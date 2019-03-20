cf88db99a2d3512fd66d92c8fce47e54bf93acdd
Blake Holaday <bholaday@purdue.edu>
2019-03-20 16:29:41 -0400

    Load 150-item cpt into cards and details

--- src/themes/purdue-alumni-association/page-150-objects.php
+++ src/themes/purdue-alumni-association/page-150-objects.php
@@ -13,16 +13,15 @@ if ( have_posts() ) {
         echo "<h1>", the_title(), "</h1>";
         echo "<p>", the_content(), "</p>";
         echo "</section>";
-
     }
 } else {
     get_template_part( 'template-parts/no-posts' );
 }
 
 $args = array(
-    'post_type' => 'post',
-    //'meta_key' => 'type',
-    //'meta_value' => 'object',
+    'post_type' => '150-item',
+    'meta_key' => '150_item_type',
+    'meta_value' => 'object',
     'orderby' => 'title',
     'order' => 'ASC',
     'posts_per_page' => 10
@@ -32,14 +31,18 @@ $args = array(
 $the_query = new WP_Query( $args );
 
 // The Loop
-echo "<section id=\"ajax-posts\" class=\"row row--slim\">";
+echo "<section id=\"ajax-posts\" class=\"row row--slim bootstrap-row\">";
 if ( $the_query->have_posts() ) {
     $max_num_pages = $the_query->max_num_pages;
     //echo "<div class=\"bootstrap-row p150-card-list\">";
     while ( $the_query->have_posts() ) {
         $the_query->the_post();
-        //get_template_part( 'template-parts/150-item-card' );
-        echo "<h3>",the_title(),"</h3>";
+        $counter = 0;
+        while ($counter < 8) {
+            get_template_part( 'template-parts/150-item-card' );
+            $counter++;
+        }
+        // echo "<h3>", the_title(), "</h3>";
     }
     //echo "</div>";
     wp_reset_postdata();
@@ -47,24 +50,9 @@ if ( $the_query->have_posts() ) {
     // no objects
     echo "no objects";
 }
-echo "<style>.loader {
-  border: 5px solid #f3f3f3;
-  border-top: 5px solid #555;
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  animation: spin 1s linear infinite;
-  margin: auto;
-}
-
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-}</style>";
 echo "</section>";
 
 // add template for lightbox
 ?>
 <input type="hidden" id="totalpages" value="<?= $max_num_pages ?>">
-<!-- <div id="more_posts">Load More</div> -->
-<? get_footer(); ?>
+<?php get_footer(); ?>
--- src/themes/purdue-alumni-association/template-parts/150-item-card.php
+++ src/themes/purdue-alumni-association/template-parts/150-item-card.php
@@ -1,30 +1,11 @@
-<!-- <div class="col-xs-12 col-sm-6 col-md-4">
-    <div class="p150-item-card">
-        <div class="p150-item-card__image-container">
-            <?php /*echo get_the_post_thumbnail(
-                $post_id,
-                'Medium-Large',
-                array(
-                    'class' => 'p150-item-card__image',
-                    'width' => '500',
-                    'height' => '500'
-                )
-            ); */?>
+<div class="col-xs-12 col-sm-6 col-md-4">
+    <a href="<?= get_the_permalink(); ?>" data-featherlight="ajax" class="card card--150-item card--fadeinup">
+        <?php
+        $image = rwmb_meta( '150_item_thumbnail' );
+        echo '<img class="card__image lazy" src="http://localhost:8888/paa/wp-content/uploads/PAlogo_V_Black_Web100.jpg" data-src="', $image['full_url'], '" data-srcset="', $image['full_url'], ' 1x" alt="', $image['alt'], '" width=\"300\" height=\"300\">';
+        ?>
+        <div class="card__content card__content--150-item">
+            <h3 class="card__title card__title--150-item"><?php the_title(); ?></h3>
         </div>
-        <h3 class="p150-item-card__title"><?php //the_title(); ?></h3>
-    </div>
-</div> -->
-<a href="#">
-    <div class="card">
-        <?php echo get_the_post_thumbnail(
-            $post_id,
-            'Medium-Large',
-            array(
-                'class' => 'card__image'
-            )
-        ); ?>
-        <div class="card__content">
-            <h3 class="card__title"><?php the_title(); ?></h3>
-        </div>
-    </div>
-</a>
+    </a>
+</div>
