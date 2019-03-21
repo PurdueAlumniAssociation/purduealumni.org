62422b33a23f47bc6d29be82ac32f6c1a3610df0
Blake Holaday <bholaday@purdue.edu>
2019-03-21 13:18:59 -0400

    Refactor, strip out some html

--- src/themes/purdue-alumni-association/page-blank.php
+++ src/themes/purdue-alumni-association/page-blank.php
@@ -5,32 +5,35 @@
 ?>
 <?php get_header(); ?>
 <?php
-    if ( have_posts() ) : while ( have_posts() ) : the_post();
+    if ( have_posts() ) {
+        while ( have_posts() ) {
+            the_post();
 
-        get_template_part( 'template-parts/web-banner' );
+            get_template_part( 'template-parts/web-banner' );
 
-        if ( is_active_sidebar( 'left-sidebar' ) ) : ?>
-            <section class="row">
-                <div class="layout">
-                    <div class="layout__main">
-                        <?php get_template_part( 'page-content-blank' ); ?>
+            if ( is_active_sidebar( 'left-sidebar' ) ) { ?>
+                <section class="row">
+                    <div class="layout">
+                        <div class="layout__main">
+                            <?php get_template_part( 'page-content-blank' ); ?>
+                        </div>
+                        <aside class="layout__sidebar">
+                            <?php dynamic_sidebar( 'left-sidebar' ); ?>
+                        </aside>
                     </div>
-                    <aside class="layout__sidebar">
-                        <?php dynamic_sidebar( 'left-sidebar' ); ?>
-                    </aside>
-                </div>
-            </section>
-        <?php else : ?>
-            <section class="row">
-                <?php get_template_part( 'page-content-blank' ); ?>
-            </section>
-        <?php endif; ?>
-<?php endwhile; else : ?>
+                </section>
+            <?php
+            } else {
+                get_template_part( 'page-content-blank' );
+            }
+        }
+    } else { ?>
     <section class="row">
         <main id="main" tabindex="-1">
             <h1>Oh no!</h1>
             <p>Sorry, something went wrong.</p>
         </main>
     </section>
-<?php endif; ?>
+<?php
+    } ?>
 <?php get_footer(); ?>
