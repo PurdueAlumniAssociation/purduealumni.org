430567c5aa35ff016be4e7ce0aed835bfe2c340b
Blake Holaday <bholaday@purdue.edu>
2019-03-22 08:45:53 -0400

    Allow 'item-type' as query var

--- src/themes/purdue-alumni-association/function-includes/custom-query-vars.php
+++ src/themes/purdue-alumni-association/function-includes/custom-query-vars.php
@@ -1,6 +1,7 @@
 <?php
 function paa_add_query_vars_filter( $vars ) {
-  $vars[] = "trip-year";
-  return $vars;
+    $vars[] = "trip-year";
+    $vars[] = "item-type";
+    return $vars;
 }
 add_filter( 'query_vars', 'paa_add_query_vars_filter' );
