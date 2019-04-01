dd40be4c5c2a8daade408ccd02e4188c111b1945
Blake Holaday <bholaday@purdue.edu>
2019-04-01 16:23:36 -0400

    Only call ajax if the href is set

--- src/themes/purdue-alumni-association/js/150-item-detail.js
+++ src/themes/purdue-alumni-association/js/150-item-detail.js
@@ -1,18 +1,15 @@
 $(".p150-item-detail__previous, .p150-item-detail__next").click( function( event ) {
     event.preventDefault();
-    console.log(event.target.href);
 
-    $.ajax({
-        url: event.target.href,
-        success: function(result) {
-            console.log("Success");
-            $(".featherlight-inner").html(result);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log("Error: " + textStatus);
-        },
-        complete: function() {
-            console.log("Complete");
-        }
-    });
+    if ( event.target.href !== undefined ) {
+        $.ajax({
+            url: event.target.href,
+            success: function(result) {
+                $(".featherlight-inner").html(result);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log("Error: " + textStatus);
+            }
+        });
+    }
 });
