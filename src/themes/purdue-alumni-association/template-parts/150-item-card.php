aad98f980f15af233bc2935d782b4c23be87d61d
Blake Holaday <bholaday@purdue.edu>
2019-04-09 11:02:36 -0400

    Update path of default image

--- src/themes/purdue-alumni-association/template-parts/150-item-card.php
+++ src/themes/purdue-alumni-association/template-parts/150-item-card.php
@@ -8,7 +8,7 @@ $data_categories_string = implode( " ", $item_terms );
     <a href="<?= get_the_permalink(); ?>" data-featherlight="ajax" class="card card--150-item card--fadeinup">
         <?php
         $image = rwmb_meta( '150_item_thumbnail' );
-        echo '<img class="card__image lazy" src="http://localhost:8888/paa/wp-content/uploads/PAlogo_V_Black_Web100.jpg" data-src="', $image['full_url'], '" data-srcset="', $image['full_url'], ' 1x" alt="', $image['alt'], '" width="300" height="300">';
+        echo '<img class="card__image lazy" src="http://www.purduealumni.com/wp-content/uploads/PAlogo_V_Black_Web100.jpg" data-src="', $image['full_url'], '" data-srcset="', $image['full_url'], ' 1x" alt="', $image['alt'], '" width="300" height="300">';
         ?>
         <div class="card__content card__content--150-item">
             <h3 class="card__title card__title--150-item"><?php the_title(); ?></h3>
