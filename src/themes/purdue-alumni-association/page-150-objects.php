e83b6122b094af6209f34ea0e4f76982bd808644
Blake Holaday <bholaday@purdue.edu>
2019-04-12 14:54:57 -0400

    New template

---
+++ src/themes/purdue-alumni-association/page-150-objects.php
@@ -0,0 +1,76 @@
+<?php
+/*
+    Template Name: 150 Objects
+*/
+?>
+<?php get_header();
+
+$taxonomy = '150-category';
+$terms = get_terms($taxonomy);
+
+?>
+<section class="row row--no-padding">
+    <img class="alignnone size-full wp-image-2395 banner" src="https://dev.purduealumni.org/wp-content/uploads/web-banner_150-objects.jpg" srcset="<?= wp_get_attachment_image_srcset(2395) ?>" alt="Object Permanence" width="1440" height="355" />
+</section>
+
+<?php
+if ( have_posts() ) {
+    while ( have_posts() ) {
+        the_post();
+        if ( is_active_sidebar( 'left-sidebar' ) ) { ?>
+        <section class="row">
+            <main class="" id="main" tabindex="-1">
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+            </main>
+        </section>
+    <?php
+        }
+    }
+}
+?>
+<!-- <section class="row row--slim">
+    <div class="items-intro">
+        <p class="items-intro__tagline">As Purdue University marks its sesquicentennial on May 6, 2019, we share the history of the University — stories both historic and modern — as told through 150 objects.</p>
+        <p class="items-intro__credit">Compiled by Kat Braz (LA’01) and Joel Meredith</p>
+        <hr class="items-intro__divider" />
+    </div>
+</section> -->
+<section class="row" style="padding-bottom: .3em; text-align: right;">
+    <h1 class="sr-only">150 Objects</h1>
+    <label for="category-select">Filter: </label>
+    <select id="category-select">
+        <option value="all">All</option>
+        <?php foreach( $terms as $category ) { ?>
+            <option value="<?= $category->slug ?>"><?= $category->name ?></option>
+        <?php } ?>
+    </select>
+</section>
+<?php
+
+// query
+$the_query = new WP_Query(array(
+   'post_type'         => '150-item',
+   'posts_per_page'    => -1,
+   'orderby'           => 'title',
+   'order'             => 'ASC'
+));
+
+echo "<section class=\"row row--slim bootstrap-row\">";
+if ( $the_query->have_posts() ) {
+    $cards = array();
+    $lightboxes = array();
+
+    while ( $the_query->have_posts() ) {
+        $the_query->the_post();
+        //get_template_part( 'template-parts/150-item-card' );
+        get_template_part( 'template-parts/150-item-card' );
+        get_template_part( 'template-parts/150-item-lightbox' );
+    }
+} else {
+    get_template_part( 'template-parts/no-posts' );
+}
+echo "</section>";
+?>
+
+<?php get_footer(); ?>
