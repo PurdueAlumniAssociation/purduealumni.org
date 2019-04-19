f216086919dfd2c714807b70597cdeb93011f37f
Blake Holaday <bholaday@purdue.edu>
2019-04-19 16:07:45 -0400

    Add template for membership call
    Remove unused code blocks

--- src/themes/purdue-alumni-association/page-150-objects.php
+++ src/themes/purdue-alumni-association/page-150-objects.php
@@ -5,37 +5,19 @@
 ?>
 <?php get_header();
 
-$taxonomy = '150-category';
-$terms = get_terms($taxonomy);
+$terms = get_terms( '150-category' );
 
 ?>
 <section class="row row--no-padding">
     <img class="alignnone size-full wp-image-2395 banner" src="https://dev.purduealumni.org/wp-content/uploads/web-banner_150-objects.jpg" srcset="<?= wp_get_attachment_image_srcset(2395) ?>" alt="Object Permanence" width="1440" height="355" />
 </section>
-
-<?php
-if ( have_posts() ) {
-    while ( have_posts() ) {
-        the_post();
-        if ( is_active_sidebar( 'left-sidebar' ) ) { ?>
-        <section class="row">
-            <main class="" id="main" tabindex="-1">
-                <h1><?php the_title(); ?></h1>
-                <?php the_content(); ?>
-            </main>
-        </section>
-    <?php
-        }
-    }
-}
-?>
-<!-- <section class="row row--slim">
+<section class="row row--slim">
     <div class="items-intro">
         <p class="items-intro__tagline">As Purdue University marks its sesquicentennial on May 6, 2019, we share the history of the University — stories both historic and modern — as told through 150 objects.</p>
         <p class="items-intro__credit">Compiled by Kat Braz (LA’01) and Joel Meredith</p>
         <hr class="items-intro__divider" />
     </div>
-</section> -->
+</section>
 <section class="row" style="padding-bottom: .3em; text-align: right;">
     <h1 class="sr-only">150 Objects</h1>
     <label for="category-select">Filter: </label>
@@ -56,21 +38,47 @@ $the_query = new WP_Query(array(
    'order'             => 'ASC'
 ));
 
-echo "<section class=\"row row--slim bootstrap-row\">";
+echo '<section class="row row--slim bootstrap-row">';
 if ( $the_query->have_posts() ) {
-    $cards = array();
-    $lightboxes = array();
-
     while ( $the_query->have_posts() ) {
         $the_query->the_post();
-        //get_template_part( 'template-parts/150-item-card' );
         get_template_part( 'template-parts/150-item-card' );
-        get_template_part( 'template-parts/150-item-lightbox' );
     }
 } else {
     get_template_part( 'template-parts/no-posts' );
 }
 echo "</section>";
-?>
 
+if ( $the_query->have_posts() ) {
+    while ( $the_query->have_posts() ) {
+        $the_query->the_post();
+        get_template_part( 'template-parts/150-item-lightbox' );
+    }
+}
+
+
+?>
+<div class="p150-lightbox-background p150-lightbox-background--membership" data-lightbox-id="99999">
+    <div class="p150-lightbox">
+        <a href="#" class="p150-lightbox__close-button"><i class="fas fa-times" aria-hidden></i><span class="sr-only">close</span></a>
+        <div class="p150-item-detail">
+            <div class="p150-item-detail__flex">
+                <div class="p150-item-detail__image-container">
+                    <img src="http://via.placeholder.com/500x500" />
+                </div>
+                <div class="p150-item-detail__content-container">
+                    <h3 class="p150-item-detail__title">Become a Member</h3>
+                    <div class="p150-item-detail__description">
+                        <p>Become a member. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <a href="https://www.purduealumni.org/membership/membership-plans" class="button button--gold button--bold">Join Today</a>
+                    </div>
+                </div>
+            </div>
+            <div class="p150-item-detail__mobile-nav">
+                <a class="p150-item-detail__previous" href="#"><i class="fas fa-chevron-left p150-item-detail__previous-icon"></i>Previous</a>
+                <a class="p150-item-detail__next" href="#">Next<i class="fas fa-chevron-right p150-item-detail__next-icon"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
 <?php get_footer(); ?>
