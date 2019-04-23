221a065f5977859d31a0e8fc51e949042d863b66
Merriken <gmerrike@1alu-dau2230-a1.csds.purdue.edu>
2019-04-23 13:37:27 -0400

    Change Login to Log In

--- src/themes/purdue-alumni-association/template-parts/login-lightbox.php
+++ src/themes/purdue-alumni-association/template-parts/login-lightbox.php
@@ -3,12 +3,12 @@
         <div class="login-box__column login-box__column--info">
             <h2 class="login-box__title">Your Information</h2>
             <p>Update your address, manage your profile, or make a payment.</p>
-            <a href="https://tinyurl.com/ybr3yvcm" class="button button--gold login-box__button">Login</a>
+            <a href="https://tinyurl.com/ybr3yvcm" class="button button--gold login-box__button">Log In</a>
         </div>
         <div class="login-box__column login-box__column--benefits">
             <h2 class="login-box__title">Membership Benefits</h2>
             <p>Access discounts, career services, or any benefit of your Purdue Alumni Association membership.</p>
-            <a href="https://purdue.vineup.com/" class="button button--teal login-box__button">Login</a>
+            <a href="https://purdue.vineup.com/" class="button button--teal login-box__button">Log In</a>
         </div>
         <div class="login-box__column login-box__column--desc">
             <h2 class="login-box__title login-box__title--desc">Why two logins?</h2>
