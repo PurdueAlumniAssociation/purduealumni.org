b70706b5685ef29c5ac2951d5efac8317ebcdfad
Blake Holaday <bholaday@purdue.edu>
2019-04-24 13:00:13 -0400

    Add validation rule to Graduating Senior Information form

--- src/themes/purdue-alumni-association/function-includes/gf-customizations.php
+++ src/themes/purdue-alumni-association/function-includes/gf-customizations.php
@@ -1,10 +1,20 @@
 <?php
 
 // exclude purdue.edu from email
-// PASE Senior Send Off Form
+// PASE Senior Send Off form
 new GWEmailDomainControl(array(
     'form_id' => 30,
     'field_id' => 2,
+    'domains' => array('purdue.edu'),
+    'validation_message' => __('Whoops! At some point Purdue will delete your <strong>%s</strong> email account, and we want to be able to stay in touch afterwards. Please provide an alternate email address.'),
+    'mode' => 'ban'
+));
+
+// exclude purdue.edu from email
+// Graduating Senior Information form
+new GWEmailDomainControl(array(
+    'form_id' => 46,
+    'field_id' => 3,
     'domains' => array('purdue.edu'),
     'validation_message' => __('Whoops! At some point Purdue will delete your <strong>%s</strong> email account, and we want to be able to stay in touch afterwards. Please provide an alternate email address.'),
     'mode' => 'ban'
