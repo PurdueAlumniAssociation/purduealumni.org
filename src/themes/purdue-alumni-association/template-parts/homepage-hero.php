2f51f1db24634b13562b67e620b8975c0e35058a
Merriken <gmerrike@1alu-dau2230-a1.csds.purdue.edu>
2019-05-21 09:07:22 -0400

    Remove unneeded code

--- src/themes/purdue-alumni-association/template-parts/homepage-hero.php
+++ src/themes/purdue-alumni-association/template-parts/homepage-hero.php
@@ -27,17 +27,7 @@ function output_hero_banner( $id ) {
         // build output
         $output = "<section class=\"row row--no-padding\">
                 <div class=\"homepage-hero\">
-                    <img class=\"homepage-hero__image\" srcset=\"${img_srcset} 320w,
-                    ${img_srcset} 360w,
-                    ${img_srcset} 375w,
-                    ${img_srcset} 411w,
-                    ${img_srcset} 414w,
-                    ${img_srcset} 768w,
-                    ${img_srcset} 800w,
-                    ${img_srcset} 1024w,
-                    ${img_srcset} 1600w,
-                    ${img_srcset} 1920w,
-                    ${img_srcset} 2400w\" src=\"${img_src}\" alt=\"${img_alt}\" />
+                    <img class=\"homepage-hero__image\" srcset=\"${img_srcset}\" src=\"${img_src}\" alt=\"${img_alt}\" />
                     ${dark_overlay}
                     <div class=\"homepage-hero__content-container\">
                         <div class=\"homepage-hero__primary\">
