a04ec71a58642bd4b0e0dbf318024d9f9845cadf
Merriken <gmerrike@1alu-dau2230-a1.csds.purdue.edu>
2019-06-12 09:35:30 -0400

    Add FAQ section to membership page

--- src/themes/purdue-alumni-association/page-membership-tiers.php
+++ src/themes/purdue-alumni-association/page-membership-tiers.php
@@ -77,5 +77,44 @@
             <a href="<?php echo esc_url( home_url( 'membership/membership-plans/detailed-plan-comparison' ) ); ?>">View the detailed comparison</a>
         </p>
     </section>
+    <section class="row">
+      <h1 class="text-center">Frequently Asked Questions</h2>
+        <div class="container">
+          <div class="bootstrap-row">
+          <div class="col-xs-12 col-sm-4">
+            <h3>Do I have to be an alumnus of Purdue University?</h3>
+            <p>No! Our membership plans and benefits are geared towards Purdue grads, but membership is not limited to graduates of the university.</p>
+          </div>
+          <div class="col-xs-12 col-sm-4">
+            <h3>How do I know which plan is right for me?</h3>
+            <p>Take a look at our <a href="https://www.purduealumni.org/membership/membership-plans/detailed-plan-comparison/">detailed comparison table</a> to find out exactly what each plan offers and which is the best choice for you.</p>
+          </div>
+          <div class="col-xs-12 col-sm-4">
+            <h3>Why are you discontinuing life membership?</h3>
+            <p>With the need to deliver relevant membership offerings at different stages of life, combined with the need to maintain flexibility in programs and services offered, the life membership business model is simply no longer feasible. However, for those looking for longer-term membership, we now offer 3- and 10-year memberships at discounted rates.</p>
+          </div>
+          <div class="col-xs-12 col-sm-4">
+            <h3>How can I cancel my membership?</h3>
+            <p>If you ever want to cancel your membership for any reason, please call us at 800-414-1541 and ask to speak with our membership team. We promise not to pester you about why you’re leaving.</p>
+          </div>
+          <div class="col-xs-12 col-sm-4">
+            <h3>Which payment methods do you accept?</h3>
+            <p>We accept all major credit cards.</p>
+          </div>
+          <div class="col-xs-12 col-sm-4">
+            <h3>Is it possible to switch to another plan after signing up?</h3>
+            <p>Yes! Our plans are designed to fit your needs and goals. If your situation changes, please call us at 800-414-1541 and ask to speak with our membership team.</p>
+          </div>
+          <div class="col-xs-12 col-sm-4">
+            <h3>How can I redeem a coupon code?</h3>
+            <p>You’ll be able to add your coupon code during the checkout process. The corresponding discount will be applied to your order.</p>
+          </div>
+          <div class="col-xs-12 col-sm-4">
+            <h3>How can I redeem a coupon code?</h3>
+            <p>You’ll be able to add your coupon code during the checkout process. The corresponding discount will be applied to your order.</p>
+          </div>
+        </div>
+      <p class= "text-center"><a href="https://www.purduealumni.org/membership/membership-plans/frequently-asked-questions/">View all FAQs</a></p>
+    </section>
 </main>
 <?php get_footer(); ?>
