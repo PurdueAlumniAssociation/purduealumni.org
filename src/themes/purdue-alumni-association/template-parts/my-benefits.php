0f421ab62c48f954f80132235c64be491877054e
Blake Holaday <bholaday@purdue.edu>
2019-09-06 12:31:38 -0400

    Abstract out class file

--- src/themes/purdue-alumni-association/template-parts/my-benefits.php
+++ src/themes/purdue-alumni-association/template-parts/my-benefits.php
@@ -1,46 +1,5 @@
 <?php
-class Benefit
-{
-    private $title;
-    private $plans;
-    private $public_description;
-    private $member_description;
-    private $public_url;
-    private $member_url;
-    private $member_url_link_text = "Access Benefit";
-
-    public function __construct($title, $plans, $member_url)
-    {
-        $this->title = $title;
-        $this->plans = $plans;
-        $this->member_url = $member_url;
-    }
-
-    public function get_the_title()
-    {
-        return $this->title;
-    }
-
-    public function get_the_plans()
-    {
-        return $this->plans;
-    }
-
-    public function the_title()
-    {
-        echo $this->title;
-    }
-
-    public function get_the_member_link()
-    {
-        return "<a href=\"{$this->member_url}\">{$this->member_url_link_text}</a>";
-    }
-
-    public function set_member_link_text($string)
-    {
-        $this->member_url_link_text = htmlspecialchars($string);
-    }
-}
+require_once( get_template_directory() . '/classes/Benefit.class.php' );
 
 // $memberships = wc_memberships_get_user_active_memberships();
 //
