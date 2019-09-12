3c604d75297818f8cd41e920e4191e18fec836c0
Blake Holaday <bholaday@purdue.edu>
2019-09-12 09:41:42 -0400

    Remove descriptive text

--- src/themes/purdue-alumni-association/woocommerce/myaccount/my-address.php
+++ src/themes/purdue-alumni-association/woocommerce/myaccount/my-address.php
@@ -43,7 +43,7 @@ $col    = 1;
 ?>
 <h3>Addresses</h3>
 <p>
-	<?php echo apply_filters( 'woocommerce_my_account_my_address_description', esc_html__( 'The following addresses will be used on the checkout page by default.', 'woocommerce' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+	<?php echo apply_filters( 'woocommerce_my_account_my_address_description', esc_html__( '', 'woocommerce' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 </p>
 
 <?php if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) : ?>
