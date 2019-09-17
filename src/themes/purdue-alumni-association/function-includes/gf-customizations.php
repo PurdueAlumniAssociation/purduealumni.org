cc9062c5986b91d48a5aeaf571e0734226f59f04
Blake Holaday <bholaday@purdue.edu>
2019-09-17 10:20:30 -0400

    Include email link with prefilled subject and body

--- src/themes/purdue-alumni-association/function-includes/gf-customizations.php
+++ src/themes/purdue-alumni-association/function-includes/gf-customizations.php
@@ -47,8 +47,8 @@ add_filter( 'gform_paypalpaymentspro_args_before_payment', 'paa_include_paypal_c
 function paa_change_message( $message, $id ){
     $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']
             === 'on' ? "https" : "http") . "://" .
-            $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+            $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
-    return "Oops! We could not find the form. <a href=\"mailto:alumnidigital@purdue.edu?subject=Form Not Found&body=There is a broken form here: {$link}\">Please let us know</a>.";
+    return "Oops! We could not find the form. <a href=\"mailto:alumnidigital@purdue.edu?subject=Form Not Found&body=There is a broken form here: {$link}\">Please let us know</a>, and we'll get it fixed right away.";
 }
 add_filter( 'gform_form_not_found_message', 'paa_change_message', 10, 2 );
