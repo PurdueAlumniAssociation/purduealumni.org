2ef9e5b9604548b0ef10a3ca57ee22120ce6ebcc
Blake Holaday <bholaday@purdue.edu>
2019-10-11 14:08:51 -0400

    Get all communities

---
+++ src/themes/purdue-alumni-association/template-parts/club-index.php
@@ -0,0 +1,31 @@
+<?php
+
+// get all communities
+$args = array(
+    'post_type' => 'community',
+    'post_status' => 'any',
+    'posts_per_page' => -1,
+    'nopaging' => true,
+);
+$the_query = new WP_Query( $args );
+
+// The Loop
+if ( $the_query->have_posts() ) {
+    echo '<ul>';
+    while ( $the_query->have_posts() ) {
+        $the_query->the_post();
+        $community_id = get_the_ID();
+        echo '<li>' . rwmb_meta( 'community__city' ) . ", "  . rwmb_meta( 'community__state' ) . ", " . rwmb_meta( 'community__country' ) . '</li>';
+
+    }
+    echo '</ul>';
+} else {
+    // no posts found
+}
+/* Restore original Post Data */
+wp_reset_postdata();
+
+// filter by type
+// sort alphabetically by country
+// sort alphabetically by city
+// display in a table
