eaa11d4c9ed0fb7a2e8be25f029aff4bb2054b0d
Blake Holaday <bholaday@purdue.edu>
2019-10-11 09:09:59 -0400

    Add user logic checks

---
+++ src/themes/purdue-alumni-association/template-parts/club-dashboard.php
@@ -0,0 +1,18 @@
+<h1><?php the_title(); ?></h1>
+
+<?php
+
+if (is_user_logged_in()) {
+    $volunteer = get_user_meta( get_current_user_id(), 'community_volunteer', TRUE );
+
+    if ( ! empty($volunteer) ) {
+        the_content(); ?>
+        <iframe id="club-dashboard" src="https://tableau.itap.purdue.edu/views/EntityMapNEW/Overview?:embed=y&amp;:showShareOptions=true&amp;:display_count=no&amp;:showVizHome=no"></iframe>
+        <?php
+    } else {
+        echo "Sorry! You must be a club volunteer to view the dashboard. Contact <a href=\"mailto:alumnidigital@purdue.edu?subject=I need help with the club dashboard\">alumnidigital@purdue.edu</a> if you need help.";
+    }
+} else {
+    echo "You must <a href=\"", wp_login_url( get_permalink() ), "\">log in</a> and to view the dashboard.";
+}
+?>
