d3de0129d683d8ac8f23f402ef190f93d1d235a7
Gabrielle Merriken <gabriellemerriken@gmail.com>
2019-12-04 14:04:32 -0500

    removed li type, removed '__ul'

--- src/themes/purdue-alumni-association/function-includes/shortcode-community-index.php
+++ src/themes/purdue-alumni-association/function-includes/shortcode-community-index.php
@@ -56,7 +56,7 @@ function output_community_list($atts){
   $old_location = "";
   if ( $the_query->have_posts() ) {
       $output = "";
-      $output .= '<ul class="community-list__ul">';
+      $output .= '<ul class="community-list">';
       while ( $the_query->have_posts() ) {
           $the_query->the_post();
           $community_id = get_the_ID();
@@ -80,7 +80,7 @@ function output_community_list($atts){
                   $old_location = $location;
               }
 
-              $output .= "<li type=disc><a href=\"". get_the_permalink(). "\">". get_the_title(). "</a></li>";
+              $output .= "<li class='community-list__li'><a href=\"". get_the_permalink(). "\">". get_the_title(). "</a></li>";
 
           } elseif ($type['type'] == "affinity") {
               $output .= "<li><a href=\"". get_the_permalink(). "\">". get_the_title(). "</a></li>";
