9176b7ea16ced2ff607b1ab503e1156b53e5193d
Blake Holaday <bholaday@purdue.edu>
2020-01-27 10:08:19 -0500

    Add comments

--- src/themes/purdue-alumni-association/function-includes/shortcode-community-index.php
+++ src/themes/purdue-alumni-association/function-includes/shortcode-community-index.php
@@ -53,42 +53,57 @@ function output_community_list($atts){
   }
   $the_query = new WP_Query( $args );
 
-  $old_location = "";
+  $prev_location = "";
   if ( $the_query->have_posts() ) {
       $output = "";
+
+      // start the unordered list for the communities
       $output .= '<ul class="community-list">';
+
+      // loop through the results of the query
       while ( $the_query->have_posts() ) {
           $the_query->the_post();
           $community_id = get_the_ID();
 
+          // run this logic for clubs and interantional networks
           if ($type['type'] == "club" || $type['type'] == "international") {
+              // select data source based on type of community
               if ($type['type'] == "club") {
-                  $location = rwmb_meta( 'community__state' );
+                  $state_country = rwmb_meta( 'community__state' );
               } elseif ($type['type'] == "international") {
-                  $location = rwmb_meta( 'community__country' );
+                  $state_country = rwmb_meta( 'community__country' );
               }
 
-              //check for new location
-              if ($old_location != $location) {
-                  if ($old_location != "" ) {
+              // check for new state/country
+              // if the previous state/country is different than the current state/country
+              if ($prev_location != $state_country) {
+                  // if the previous state/country is not empty, close the nested list
+                  if ($prev_location != "" ) {
                       $output .= "</ul></li>";
-
                   }
+
+                  // add the state/country to the list and open a nested list for any locations inside
                   $output .= "<li class='community-list-item community-list-item--state-country'>${location}<ul>";
 
-                  $old_location = $location;
+                  // update the previous state/country with the current state/country for the next loop
+                  $prev_location = $state_country;
               }
 
+              // add the current location to the nested list
               $output .= "<li class='community-list-item community-list-item--location'><a href=\"". get_the_permalink(). "\">". get_the_title(). "</a></li>";
 
           } elseif ($type['type'] == "affinity") {
+              // add the current location to the list
               $output .= "<li><a href=\"". get_the_permalink(). "\">". get_the_title(). "</a></li>";
           }
       }
+
+      // close out the last nested list, list item, and unordered list
       $output .= '</ul></li></ul>';
+
       return $output;
   } else {
-      // no posts found
+      // no communities found
       $no_community = "no communities";
       return $no_community;
   }
