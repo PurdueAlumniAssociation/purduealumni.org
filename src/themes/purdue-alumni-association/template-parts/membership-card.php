042cb460e13dd2b73d5b50799a664e4a5c974c7a
Blake Holaday <bholaday@purdue.edu>
2020-01-30 14:02:33 -0500

    Adjust logic for displaying expiration

--- src/themes/purdue-alumni-association/template-parts/membership-card.php
+++ src/themes/purdue-alumni-association/template-parts/membership-card.php
@@ -8,21 +8,33 @@ $image_path = get_template_directory_uri() . "/images/blankmembershipcard.jpg";
 
 $memberships = wc_memberships_get_user_active_memberships();
 
+//echo "<pre>",print_r(get_class_methods("WC_Memberships_Integration_Subscriptions_User_Membership")),"</pre>";
+
+//echo "<pre>",print_r($memberships),"</pre>";
+
 if (!empty($memberships)) {
     $membership_levels = array();
     foreach($memberships as $membership) {
         $membership_levels[] = $membership->plan->slug;
         //echo "<pre>" . print_r($membership) . "</pre>";
-
-        $expiration_date = $membership->get_end_date();
+        if ($membership->get_status() == "active") {
+            $expiration_date = $membership->get_next_bill_on_date("m/d/Y");
+        } elseif ($membership->get_status() == "pending") {
+            $expiration_date = $membership->get_end_date("m/d/Y");
+        } else {
+            $expiration_date = "";
+        }
     }
 }
+//echo "<pre>",print_r($membership_levels),"</pre>";
 
 if( !empty($membership_levels) ) {
     if (in_array("career-max", $membership_levels)) {
         $effective_membership_level = "Career Max";
     } elseif (in_array("professional", $membership_levels)) {
         $effective_membership_level = "Professional";
+    } elseif (in_array("life", $membership_levels)) {
+        $effective_membership_level = "Life";
     } elseif (in_array("plus", $membership_levels)) {
         $effective_membership_level = "Plus";
     } elseif (in_array("basic", $membership_levels)) {
@@ -30,10 +42,19 @@ if( !empty($membership_levels) ) {
     } elseif (in_array("pase", $membership_levels)) {
         $effective_membership_level = "PASE";
     }
+
+    if ( $effective_membership_level == "Life" ) {
+        $display_expiration_date = "Never Expires";
+    } elseif ( ! empty($expiration_date) ) {
+        $display_expiration_date = "Expires " . $expiration_date;
+    } else {
+        $display_expiration_date = "";
+    }
+
     ?>
     <div class="membership-card">
         <span class="membership-card__type"><?= $effective_membership_level ?> Member</span>
-        <span class="membership-card__expiration">Expires 01/01/2020<?= $expiration_date ?></span>
+        <span class="membership-card__expiration"><?= $display_expiration_date ?></span>
         <span class="membership-card__name"><?= $user_full_name ?></span>
         <img class="membership-card__image" src="<?= $image_path ?>" />
     </div>
@@ -87,6 +108,11 @@ if( !empty($membership_levels) ) {
         font-size: 1.2em;
         top: 15px;
         left: 15px;
+    }
+    .membership-card__expiration {
+        font-size: .8em;
+        top: 35px;
+        left: 16px;
     }
     .membership-card__name {
         font-size: .7em;
