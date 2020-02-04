70d774dcc90c1cfa0887b3bdaa9fa0ef7a44c30a
Blake Holaday <bholaday@purdue.edu>
2020-02-04 12:23:24 -0500

    make widgets an include

--- src/themes/purdue-alumni-association/functions.php
+++ src/themes/purdue-alumni-association/functions.php
@@ -68,54 +68,7 @@ function paa_register_menus() {
 }
 add_action( 'init', "paa_register_menus" );
 
-/**
- * Register our sidebars and widgetized areas.
- *
- */
-function paa_widgets_init() {
-    register_sidebar( array(
-        'name'          => 'Left Sidebar',
-        'id'            => 'left-sidebar',
-        'before_title'  => '<h2 class="sr-only">',
-        'after_title'   => '</h2>',
-        'before_widget' => '<div id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</div>'
-    ) );
-    register_sidebar( array(
-        'name'          => 'Travel Sidebar',
-        'id'            => 'travel-sidebar',
-        'before_title'  => '<h2 class="sr-only">',
-        'after_title'   => '</h2>',
-        'before_widget' => '<div id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</div>'
-    ) );
-    register_sidebar( array(
-        'name'          => 'Primary Footer - Column 1',
-        'id'            => 'primary-footer-col-1',
-        'before_widget' => '<div class="primary-footer__column">',
-        'after_widget'  => '</div>'
-    ) );
-    register_sidebar( array(
-        'name'          => 'Primary Footer - Column 2',
-        'id'            => 'primary-footer-col-2',
-        'before_widget' => '<div class="primary-footer__column">',
-        'after_widget'  => '</div>'
-    ) );
-    register_sidebar( array(
-        'name'          => 'Primary Footer - Column 3',
-        'id'            => 'primary-footer-col-3',
-        'before_widget' => '<div class="primary-footer__column">',
-        'after_widget'  => '</div>'
-    ) );
-    register_sidebar( array(
-        'name'          => 'Primary Footer - Column 4',
-        'id'            => 'primary-footer-col-4',
-        'before_widget' => '<div class="primary-footer__column">',
-        'after_widget'  => '</div>'
-    ) );
-
-}
-add_action( 'widgets_init', 'paa_widgets_init' );
+include 'function-includes/widgets.php';
 
 function add_search_box_to_menu( $items, $args ) {
     if( $args->theme_location == 'primary-menu-mobile' ) {
