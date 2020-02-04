293d2c2d847471e144c95a96db83df498a985ad8
Blake Holaday <bholaday@purdue.edu>
2020-02-04 12:24:57 -0500

    Use bootstrap for layout

--- src/themes/purdue-alumni-association/function-includes/widgets.php
+++ src/themes/purdue-alumni-association/function-includes/widgets.php
@@ -23,25 +23,25 @@ function paa_widgets_init() {
     register_sidebar( array(
         'name'          => 'Primary Footer - Column 1',
         'id'            => 'primary-footer-col-1',
-        'before_widget' => '<div class="primary-footer__column">',
+        'before_widget' => '<div class="col-xs-12 col-sm-3 primary-footer__column">',
         'after_widget'  => '</div>'
     ) );
     register_sidebar( array(
         'name'          => 'Primary Footer - Column 2',
         'id'            => 'primary-footer-col-2',
-        'before_widget' => '<div class="primary-footer__column">',
+        'before_widget' => '<div class="col-xs-12 col-sm-3 primary-footer__column">',
         'after_widget'  => '</div>'
     ) );
     register_sidebar( array(
         'name'          => 'Primary Footer - Column 3',
         'id'            => 'primary-footer-col-3',
-        'before_widget' => '<div class="primary-footer__column">',
+        'before_widget' => '<div class="col-xs-12 col-sm-3 primary-footer__column">',
         'after_widget'  => '</div>'
     ) );
     register_sidebar( array(
         'name'          => 'Primary Footer - Column 4',
         'id'            => 'primary-footer-col-4',
-        'before_widget' => '<div class="primary-footer__column">',
+        'before_widget' => '<div class="col-xs-12 col-sm-3 primary-footer__column">',
         'after_widget'  => '</div>'
     ) );
 
--- src/themes/purdue-alumni-association/template-parts/sponsors.php
+++ src/themes/purdue-alumni-association/template-parts/sponsors.php
@@ -1,11 +1,11 @@
 <div class="bootstrap-row sponsors">
     <a class="col-xs-12 col-sm-4 sponsors__link" target="_blank" rel="noopener" href="https://www.balfour.com/purdue">
-        <img class="sponsors__image" src="<?php echo esc_url( home_url( 'wp-content/uploads/balfour.svg' ) ); ?>" alt="Balfour" />
+        <img class="sponsors__image" src="" alt="Balfour" />
     </a>
     <a class="col-xs-12 col-sm-4 sponsors__link" target="_blank" rel="noopener" href="https://www.purduefed.com/personal/cards/credit-cards/">
-        <img class="sponsors__image" src="<?php echo esc_url( home_url( 'wp-content/uploads/PurdueFed18.svg' ) ); ?>" alt="PEFCU" />
+        <img class="sponsors__image" src="" alt="PEFCU" />
     </a>
     <a class="col-xs-12 col-sm-4 sponsors__link" target="_blank" rel="noopener" href="https://www.nationwide.com/purduealumni">
-        <img class="sponsors__image" src="<?php echo esc_url( home_url( 'wp-content/uploads/nationwide-logo.svg' ) ); ?>" alt="Nationwide" />
+        <img class="sponsors__image" src="" alt="Nationwide" />
     </a>
 </div>
