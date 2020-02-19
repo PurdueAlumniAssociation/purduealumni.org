150dd4c5096f257900228aaba213b1c0d1d56f23
Blake Holaday <bholaday@purdue.edu>
2020-02-19 15:37:42 -0500

    add logic for delayed membership

--- src/themes/purdue-alumni-association/template-parts/woocommerce/my-account/inactive-membership-message.php
+++ src/themes/purdue-alumni-association/template-parts/woocommerce/my-account/inactive-membership-message.php
@@ -1,26 +1,19 @@
 <?php
 
 // check for different memebrship statuses
-// $memberships = wc_memberships_get_user_memberships();
-//
-// foreach ( $memberships as $membership ) {
-//     $status[] = $membership->status;
-// }
-//
-// //print_r($status);
-//
-// // wcm-active
-// // wcm-cancelled
-// // wcm-complimentary
-// // wcm-delayed
-// // wcm-expired
-// // wcm-paused
-// // wcm-pending
-//
-// if ( in_array("wcm-cancelled", $status) || in_array("wcm-expired", $status) ) {
-//
-// }
+$memberships = wc_memberships_get_user_memberships();
 
+$message = "<p>You don't have an active membership! <a href=\"https://www.purduealumni.org/membership/\">Purchase a new mebership</a>.</p>";
+
+foreach ( $memberships as $membership ) {
+    echo "<pre>", print_r(get_class_methods($membership)), "</pre>";
+
+    if ( $membership->status == "wcm-delayed" ) {
+        $start_date = date("F j, Y", strtotime($membership->get_start_date()));
+        $message = "<p>Your membership is not active yet. Check back on or after {$start_date}.</p>";
+    }
+}
+
+echo $message;
 ?>
-<p>You don't have an active membership! <a href="https://www.purduealumni.org/membership/">Purchase a new mebership</a>.</p>
 <p>If this is an error, please let us know by emailing <a href="mailto:alumnidigital@purdue.edu">alumnidigital@purdue.edu</a></p>
