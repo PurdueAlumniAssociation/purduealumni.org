9ed75ae929dc1425199fe43ba17c83fd10c04ea3
Blake Holaday <bholaday@purdue.edu>
2020-02-27 10:13:04 -0500

    remove object permanence template case

--- src/themes/purdue-alumni-association/functions.php
+++ src/themes/purdue-alumni-association/functions.php
@@ -16,11 +16,6 @@ function paa_scripts_and_styles() {
         case "page-membership-tiers.php":
             wp_enqueue_style( 'page-membership-tiers', get_template_directory_uri() . '/css/page-membership-tiers.css' );
             break;
-        case "page-object-permanence.php":
-            wp_enqueue_style( '150-objects-styles', get_template_directory_uri() . '/css/150-objects.css' );
-            wp_enqueue_script( '150-objects-scripts', get_template_directory_uri() . '/js/150-objects.js', array('jquery'), '1.0.0', true ); // true adds it to the footer
-            wp_enqueue_script( '150-objects-sharing', '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5cab956bbac2fc69', true ); // true adds it to the footer
-            break;
         case "page-business-directory.php":
             wp_enqueue_style( 'page-biz-dir', get_template_directory_uri() . '/css/page-business-directory.css' );
             break;
