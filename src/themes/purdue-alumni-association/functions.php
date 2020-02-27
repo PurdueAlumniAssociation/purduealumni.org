ad93d13b1805935df1d9944dde3514b8082d5b9f
Blake Holaday <bholaday@purdue.edu>
2020-02-27 10:13:24 -0500

    remove 150 innovations template case

--- src/themes/purdue-alumni-association/functions.php
+++ src/themes/purdue-alumni-association/functions.php
@@ -19,10 +19,6 @@ function paa_scripts_and_styles() {
         case "page-business-directory.php":
             wp_enqueue_style( 'page-biz-dir', get_template_directory_uri() . '/css/page-business-directory.css' );
             break;
-        case "page-150-innovations-section.php":
-        case "page-150-innovations.php":
-            wp_enqueue_style( '150-innovations', get_template_directory_uri() . '/css/150-innovations.css' );
-            break;
         case "page-pase-scholarship.php":
             wp_enqueue_script( 'pase-scholarship-scripts', get_template_directory_uri() . '/js/pase-scholarship.js', array('jquery'), '1.0.0', true );
             wp_enqueue_style( 'common-styles', get_template_directory_uri() . '/style.css' );
